ed29378aa410787cb341d9372447d2c938c01a5e
Kymberlee Hill <kymberleehill@Kymberlees-MacBook-Pro.local>
2018-08-28 14:32:05 -0400

    view layout complete

---
+++ Tipster/Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Kymberlee Hill on 8/28/18.
+//  Copyright © 2018 Kymberlee Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
